83d08ef92def7c0bd85ff528f73a5eecd14afa03
Miljan Ignjatovic <miljan.ignjatovic.96@gmail.com>
2017-06-03 22:51:00 +0200

    Sredjeno upisivanje novih logova.

--- Menjacnica API klijent/src/util/LogWriter.java
+++ Menjacnica API klijent/src/util/LogWriter.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -8,6 +9,7 @@ import java.util.LinkedList;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 
 import domain.Log;
@@ -18,9 +20,13 @@ public class LogWriter {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
 		JsonArray jsonArray = new JsonArray();
-
+		
 		try {
-			FileWriter writer = new FileWriter("data/log.json", true);
+			FileReader in = new FileReader("data/log.json");
+			jsonArray = gson.fromJson(in, JsonArray.class);
+			in.close();
+			
+			FileWriter writer = new FileWriter("data/log.json");
 			for (Log l : logLst) {
 				JsonObject json = new JsonObject();
 				json.addProperty("datumVreme", dateFormater.format(l.getDatumVreme()));
@@ -30,7 +36,7 @@ public class LogWriter {
 
 				jsonArray.add(json);
 			}
-			writer.write(gson.toJson(jsonArray) + ",\n");
+			writer.write(gson.toJson(jsonArray));
 			writer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
